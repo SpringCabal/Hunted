21a6509fa33c18f13f701aaf6dbba7dc70bb9579
Gajo Petrovic <gajo01@gmail.com>
2015-08-08 07:03:51 +0900

    initial commit

---
+++ ModInfo.lua
@@ -0,0 +1,14 @@
+local modinfo = {
+	name = "Hunted",
+	shortname = "HHH",
+	game = "Hunted",
+	shortgame = "HHH",
+	description = "Hunted LD33 prep game",
+	
+	modtype = "1",
+	depend = {
+		"cursors.sdz",
+	}
+}
+
+return modinfo
