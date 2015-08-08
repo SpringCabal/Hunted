e37d2363afbd64850397eedabebaeb69c4e78733
GoogleFrog <googl3frog@gmail.com>
2015-08-08 14:20:45 +1000

    Added chili (latest from repo).

--- LuaUI/Widgets/api_chili.lua
+++ LuaUI/Widgets/api_chili.lua
@@ -31,7 +31,14 @@ local tf
 --------------------------------------------------------------------------------
 -- Chili's location
 
-CHILI_DIRNAME = "libs/chiliui/luaui/chili/chili/"
+local function GetDirectory(filepath)
+	return filepath and filepath:gsub("(.*/)(.*)", "%1")
+end
+
+assert(debug)
+local source = debug and debug.getinfo(1).source
+local DIR = GetDirectory(source) or ((LUA_DIRNAME or LUAUI_DIRNAME) .."Widgets/")
+CHILI_DIRNAME = DIR .. "chili/"
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
