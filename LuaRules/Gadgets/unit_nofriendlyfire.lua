f435d596b8f79d774b65e415f3e4979847f80dce
Eshed <ashdnazg@gmail.com>
2015-08-08 21:40:07 +0300

    forgot to add file

---
+++ LuaRules/Gadgets/unit_nofriendlyfire.lua
@@ -0,0 +1,62 @@
+-- $Id: unit_nofriendlyfire.lua 3171 2008-11-06 09:06:29Z det $
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+function gadget:GetInfo()
+  return {
+    name      = "No Friendly Fire",
+    desc      = "Adds the nofriendlyfire custom param",
+    author    = "quantum",
+    date      = "June 24, 2008",
+    license   = "GNU GPL, v2 or later",
+    layer     = -999,
+    enabled   = true  --  loaded by default?
+  }
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+if (not gadgetHandler:IsSyncedCode()) then
+  return false  --  silent removal
+end
+
+local wantedWeaponList = {}
+
+local noFFWeaponDefs = {}
+for wdid, wd in pairs(WeaponDefs) do
+	if wd.customParams and wd.customParams.nofriendlyfire then
+		noFFWeaponDefs[wdid] = true
+		wantedWeaponList[#wantedWeaponList + 1] = wdid
+	end
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+-- Automatically generated local definitions
+
+local spAreTeamsAllied = Spring.AreTeamsAllied
+local spGetUnitHealth  = Spring.GetUnitHealth
+local spSetUnitHealth  = Spring.SetUnitHealth
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+function gadget:UnitPreDamaged_GetWantedWeaponDef()
+	return wantedWeaponList
+end
+
+function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, 
+                            weaponID, attackerID, attackerDefID, attackerTeam)
+	if weaponID and noFFWeaponDefs[weaponID] then
+		if attackerID ~= unitID and unitTeam ~= Spring.GetGaiaTeamID() then
+			return 0, 0
+		end
+	end
+  
+	return damage
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
